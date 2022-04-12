c829b9df78b1f1656ddce67308a6a5459dec3ceb
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-04-12 20:07:29 +0200

    all removed

--- my-app/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function(props) {
-    return (
-        <header>
-            <h1>Hola {props.name}</h1>
-        </header>
-    );
-}
--- my-app/src/components/Listado.js
+++
@@ -1,8 +0,0 @@
-export default function(props) {
-    return (
-        <article>
-            <h2>{props.name}</h2>
-            <p>{props.description}</p>
-        </article>
-    );
-}
